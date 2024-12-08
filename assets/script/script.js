db6874248e40c28640d6c2ea4ddbca51ca051e0c
Mazen Mohamed <124138194+MazenBeso@users.noreply.github.com>
2024-12-08 22:20:17 +0200

    Add files via upload

--- assets/script/script.js
+++ assets/script/script.js
@@ -100,3 +100,21 @@ interval3 = setInterval(function(){
         clearInterval(interval3);
     }
 },80)
+
+
+const scrollToTopBtn = document.getElementById('scrollToTopBtn');
+
+window.onscroll = function() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+};
+
+scrollToTopBtn.addEventListener('click', function() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
